1a2b270be14212a7d5e11d0a581dbb130b50b925
denis <denis.guzeyev@4c-ag.de>
2017-03-09 14:32:19 +0100

    +

--- rabbitmq_task/utils.py
+++ rabbitmq_task/utils.py
@@ -3,18 +3,18 @@
 import time
 
 def init_connection(connector, host, port):
-    attempt = 0
-    connection = None
-    while not connection:
-        try:
-            connection = connector.BlockingConnection(connector.ConnectionParameters(host=host, port=port))
-        except Exception as e:
-            print 'Connection is unreachable because %s, attempt %s' % e, attempt
-            attempt += 1
-            time.sleep(3)
-            if attempt <= 5:
-                continue
-            else:
-                raise Exception('Limit of attempts was reached')
-        else:
-            return connection
+	attempt = 0
+	connection = None
+	while not connection:
+		try:
+			connection = connector.BlockingConnection(connector.ConnectionParameters(host=host, port=port))
+		except Exception as e:
+			print 'Connection is unreachable because', e, attempt
+			attempt += 1
+			time.sleep(3)
+			if attempt <= 5:
+				continue
+			else:
+				raise Exception('Limit of attempts was reached')
+		else:
+			return connection
